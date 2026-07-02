78ca5e34947d4144b38053e30b2559c7cd59603d
dmacca <dmacca@amazon.com>
2026-07-02 15:32:47 +0200

    fix: correct sunrise/sunset times from generalblue.com data

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='v1782999024';
+var CACHE='v1782999167';
 var URLS=['/pantelleria-2026/','/pantelleria-2026/index.html'];
 self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(names){return Promise.all(names.filter(function(n){return n!==CACHE}).map(function(n){return caches.delete(n)}))}))});
