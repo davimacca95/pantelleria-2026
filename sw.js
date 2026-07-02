493a660ff1842115ed1be2ad1ae7dc1b65da0afb
dmacca <dmacca@amazon.com>
2026-07-02 15:11:38 +0200

    fix: network-first SW strategy + skipWaiting (forces cache refresh)

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-var CACHE='v1782997822';
+var CACHE='v1782997900';
 var URLS=['/pantelleria-2026/','/pantelleria-2026/index.html'];
-self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
-self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
+self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(names){return Promise.all(names.filter(function(n){return n!==CACHE}).map(function(n){return caches.delete(n)}))}))});
+self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request)}))});
