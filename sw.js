c31a4c32bc9e139dd8b4abf97b0b88cb87d907bd
dmacca <dmacca@amazon.com>
2026-07-02 12:04:37 +0200

    I Magnifici 7 + 7 new restaurants on map

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='v1782986071';
+var CACHE='v1782986677';
 var URLS=['/pantelleria-2026/','/pantelleria-2026/index.html'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(URLS)}))});
 self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}))});
